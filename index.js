6ac5cb549cdb2d939a0c2868075464e69656defa
Avishisht <avishishtgupta@gmail.com>
2023-01-10 20:09:24 +0530

    returned to working stateeee

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
 
 
 app.use("/", (req, res) => {
