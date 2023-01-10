1757f301d714ce71fba3a1eb079160359077a8ed
Avishisht <avishishtgupta@gmail.com>
2023-01-10 20:07:10 +0530

    returned to working statee

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-
 app.use("/", (req, res) => {
   res.json({message: "Hellosss"});
 });
